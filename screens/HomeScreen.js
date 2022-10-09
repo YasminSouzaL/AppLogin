593adfd77b197953103abcf079a8699a03b34fb8
Yasmin Souza <souzasmimi@gmail.com>
2022-10-09 20:21:08 -0300

    ok

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,14 +1,52 @@
+import { useNavigation } from '@react-navigation/native';
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import { auth } from '../firebase';
 
 const HomeScreen = () => {
+    const navigation = useNavigation()
+
+    const handleSingOut = () => {
+        auth
+        .singOut()
+        .then(() => {
+            navigation.replace("Login")
+        })
+        .cath(error => alert(error.message))
+    }
+
     return (
-        <View>
-            <Text>Home</Text>
+        <View style={styles.container}>
+            <Text>Email: {auth.currentUser?.email}</Text>
+            <TouchableOpacity
+                onPress={handleSingOut}
+                style={styles.button}
+            >
+                <Text style={styles.buttonText}>Sign out</Text>
+            </TouchableOpacity>
         </View>
     );
 }
 
 export default HomeScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    button: {
+        backgroundColor: '#49efb5',
+        width: '60%',
+        padding: 15,
+        borderRadius: 10,
+        alignItems: 'center',
+        marginTop: 40
+    },
+    buttonText: {
+        color: 'white',
+        fontWeight: '700',
+        fontSize: 16,
+    },
+})
